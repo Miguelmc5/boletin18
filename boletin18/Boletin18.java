3cf2cd2d838ea82ac110fd5a96e0db1f78097662
Miguelmc5 <mmartinezcosta@danielcastelao.org>
2020-02-27 12:41:30 +0100

    bol18_1

---
+++ boletin18/Boletin18.java
@@ -0,0 +1,21 @@
+package boletin18;
+
+/**
+ *
+ * @author DANIELCASTELAO\mmartinezcosta
+ */
+public class Boletin18 {
+
+  
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Metodos obx=new Metodos();
+        obx.crearArray();
+        obx.amosar();
+        obx.reves();
+       
+        
+    }
+    
+}
