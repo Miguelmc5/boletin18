34a3740a734de7fce7b444d7774c3b8ec02b9234
Miguel Martinez Costa <56074037+Miguelmc5@users.noreply.github.com>
2020-02-27 12:47:38 +0100

    Add files via upload

---
+++ boletin18_4/src/boletin18_4/Boletin18_4.java
@@ -0,0 +1,17 @@
+package boletin18_4;
+
+/**
+ *
+ * @author DANIELCASTELAO\mmartinezcosta
+ */
+public class Boletin18_4 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Metodos a = new Metodos();
+        a.crearArrays();
+        a.amosarArrays();
+        a.calcularLetra(58432017);
+
+    }
+}
